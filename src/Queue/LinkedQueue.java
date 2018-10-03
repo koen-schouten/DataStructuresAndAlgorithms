96381788bd0c542b5fde14f0c0acaec7ae0b068b
koen-schouten <schouten_koen@hotmail.com>
2018-10-03 12:56:39 +0200

    Voeg een LinkedQueue toe.

--- src/Queue/LinkedQueue.java
+++ src/Queue/LinkedQueue.java
@@ -1,8 +1,41 @@
 package Queue;
 
+import LinkedLists.SinglyLinkedList;
+
 /**
  * Implementatie van een queue op basis van een SinglyLinkedList.
  * @param <E> de elementen in de queue
  */
-public class LinkedQueue<E> implements IQueue {
+public class LinkedQueue<E> implements IQueue<E> {
+
+    private SinglyLinkedList<E> list = new SinglyLinkedList<E>();
+
+    public LinkedQueue(){}
+
+    @Override
+    public int size() {
+        return list.size();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return list.isEmpty();
+    }
+
+    @Override
+    public void enqueue(E e) {
+        list.addLast(e);
+    }
+
+    @Override
+    public E first() {
+        return list.first();
+    }
+
+    @Override
+    public E dequeue() {
+        return list.removeFirst();
+    }
+
+
 }
