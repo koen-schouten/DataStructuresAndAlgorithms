47a56563b985ff2231a1eddeaecfdd209bda20de
koen-schouten <schouten_koen@hotmail.com>
2018-10-03 12:32:59 +0200

    Voeg een interface voor de Queue toe.

---
+++ src/Queue/IQueue.java
@@ -0,0 +1,33 @@
+package Queue;
+
+public interface IQueue<E> {
+    /**
+     * Geeft het aantal elementen in de Queue
+     * @return het aantal elementen in de Queue
+     */
+    int size();
+
+    /**
+     * Kijkt of de stack leeg is
+     * @return true wanneer de stack leeg is, anders false
+     */
+    boolean isEmpty();
+
+    /**
+     * Voegt een element toe aan de Qeueu
+     * @param e het element dat toegevoegd moet worden
+     */
+    void enqueue(E e);
+
+    /**
+     * Geeft het element dat vooraan in de Queue ligt
+     * @return E het element dat vooraan in de Queue ligt
+     */
+    E first();
+
+    /**
+     * Geeft en verwijdert het element dat vooraan in de Queue ligt
+     * @return E het element dat vooraan in de Queue ligt
+     */
+    E dequeue();
+}
