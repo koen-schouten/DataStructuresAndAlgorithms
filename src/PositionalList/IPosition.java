c7a68953b863ad2e2cdbcf3bf7f48b157be2c57d
koen-schouten <schouten_koen@hotmail.com>
2018-10-04 11:22:22 +0200

    Voeg een Documentatie toe voor de AFT IPosition en IPositionalList.

--- src/PositionalList/IPosition.java
+++ src/PositionalList/IPosition.java
@@ -1,6 +1,10 @@
 package PositionalList;
 
 public interface IPosition<E> {
-
+    /**
+     * Geeft de element op een gegeven positie
+     * @return het opgeslagen element
+     * @throws IllegalStateException wanneer de positie niet geldig is.
+     */
     E getElement() throws IllegalStateException;
 }
--- src/PositionalList/IPositionalList.java
+++ src/PositionalList/IPositionalList.java
@@ -2,27 +2,92 @@ package PositionalList;
 
 public interface IPositionalList<E> {
 
+    /**
+     * Geeft het aantal elementen in de lijst
+     * @return het aantal elementen in de lijst
+     */
     int size();
 
+    /**
+     * Test of de lijst leeg is
+     * @return true wanneer de lijst leeg is.
+     */
     boolean isEmpty();
 
+    /**
+     * Geeft de laatste positie in de lijst
+     * @return de laatste positie in de lijst
+     */
     IPosition<E> first();
 
+    /**
+     * Geeft de eerste positie in de lijst
+     * @return de eerste positie in de lijst
+     */
     IPosition<E> last();
 
+    /**
+     * Geeft de positie voor de positie p
+     * @param p een gegeven positite p
+     * @return de positie voor p
+     * @throws IllegalArgumentException
+     */
     IPosition<E> before(IPosition<E> p) throws IllegalArgumentException;
 
+    /**
+     *  Geeft de positie na de positie p
+     * @param p een gegeven positite p
+     * @return de positie na p
+     * @throws IllegalArgumentException
+     */
     IPosition<E> after(IPosition<E> p) throws IllegalArgumentException;
 
+    /**
+     * Voegt een element toe voor aan de lijst
+     * @param e de element die toegevoegd word.
+     * @return de positie van het nieuwe element
+     */
     IPosition<E> addFirst(E e);
 
+    /**
+     * Voegt een element toe achtet aan de lijst
+     * @param e de element die toegevoegd word.
+     * @return de positie van het nieuwe element
+     */
     IPosition<E> addLast(E e);
 
+    /**
+     * Voegt een element e toe voor positie p.
+     * @param p de positie
+     * @param e de element die toegevoegd word.
+     * @return de positie van het nieuwe element
+     * @throws IllegalArgumentException
+     */
     IPosition<E> addBefore(IPosition<E> p, E e) throws IllegalArgumentException;
 
+    /**
+     * Voegt een element e toe achter positie p.
+     * @param p de positie
+     * @param e het element die toegevoegd word.
+     * @return de positie van het nieuwe element
+     * @throws IllegalArgumentException
+     */
     IPosition<E> addAfter(IPosition<E> p, E e) throws IllegalArgumentException;
 
+    /**
+     * Vervangt een element op positie p met een nieuwe element e
+     * @param p de positie
+     * @param e het element die toegevoegd word.
+     * @return geeft het vervangen element
+     * @throws IllegalArgumentException
+     */
     E set(IPosition<E> p, E e) throws IllegalArgumentException;
 
+    /**
+     * Verwijder een element op positie p
+     * @param p de positie
+     * @return geeft het vervangen element
+     * @throws IllegalArgumentException
+     */
     E remove(IPosition<E> p) throws IllegalArgumentException;
 }
